f4a0423d8617393969c420a779b8cac47b983911
lockness-Ko <videolk9373@gmail.com>
2022-04-03 20:49:08 +1000

    basic working version

--- Makefile
+++ Makefile
@@ -1,20 +1,24 @@
 all: linux
 
+build: windows linux
+
 windows: clean
-	export CGO_ENABLED=0
 	export GOOS=windows
-	go build -ldflags="-extldflags=-static" -buildmode=pie .
-	strip ./tun
+	go build .
+	strip ./tun.exe
+	mv ./tun.exe ./build/tun.exe
 
 linux: clean
 	export CGO_ENABLED=0
 	export GOOS=linux
 	go build -ldflags="-extldflags=-static" -buildmode=pie .
 	strip ./tun
+	mv ./tun ./build/tun
 
 package: build
 	zip tun.zip ./tun
 
 clean:
-	-rm ./tun
+	-rm ./build/tun
+	-rm ./build/tun.exe
 	-rm ./tun.tar.gz
--- tun.go
+++ tun.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"os/user"
 	"runtime"
+	"strings"
 )
 
 func main() {
@@ -23,8 +25,68 @@ func main() {
 	}
 
 	if _, err := os.Stat(srv + "/running.lock"); err != nil {
+		runNgrok(srv)
+	}
+
+	if len(os.Args) <= 1 {
+		fmt.Println("Usage: tun <command>")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "start":
 		fmt.Println("Starting ngrok")
 		runNgrok(srv)
+	case "stop":
+		fmt.Println("Stopping ngrok")
+		stopNgrok(srv)
+	case "":
+		os.Exit(1)
+	default:
+		fmt.Println("Serving file")
+
+		splited := strings.Split(srv+"/"+os.Args[2], "/")
+
+		// Copy the [2]th argument to the srv directory
+		copy(os.Args[2], srv+"/"+splited[len(splited)-1])
+	}
+
+}
+
+func copy(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
+}
+
+func stopNgrok(srv string) {
+	// Delete the running.lock file
+	os.Remove(srv + "/running.lock")
+
+	// Kill the ngrok process
+	if runtime.GOOS == "windows" {
+		exec.Command("taskkill", "/F", "/IM", "ngrok.exe").Run()
+	} else {
+		exec.Command("pkill", "ngrok").Run()
 	}
 }
 
@@ -42,5 +104,5 @@ func runNgrok(srv string) {
 	case "linux":
 		cmd = exec.Command("ngrok", "http", "-region=au", "file://"+srv)
 	}
-	go cmd.Run()
+	cmd.Start()
 }
